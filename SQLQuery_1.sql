c8ff0075a2635f4b4f975f8fc2f5f42b7fe7c7ba
newton0182 <dim.chantz@gmail.com>
2021-03-12 15:47:26 +0100

    s

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -144,4 +144,125 @@ FROM [AdventureWorks2012].[Person].[Address];
 */
 
 
+-- Literal values and column names together
+SELECT 'A nice day' AS "Literal Value",
+ BusinessEntityID AS EmployeeID,
+ LoginID JobTitle --AS can be omittet when using an alias. Kolumn LoginID will be renamed to JobTitle, and the actual JobTitle column is ignored
+FROM HumanResources.Employee;
+
+
+
+
+--1 : BETWEEN
+SELECT CustomerID, SalesOrderID, OrderDate
+FROM Sales.SalesOrderHeader
+WHERE OrderDate BETWEEN '2005-07-02' AND '2005-07-15';
+--2
+SELECT CustomerID, SalesOrderID, OrderDate
+FROM Sales.SalesOrderHeader
+WHERE CustomerID BETWEEN 25000 AND 25005;
+--3
+SELECT BusinessEntityID, JobTitle
+FROM HumanResources.Employee
+WHERE JobTitle BETWEEN 'C' and 'E';
+
+--4 An illogical BETWEEN expression
+SELECT CustomerID, SalesOrderID, OrderDate
+FROM Sales.SalesOrderHeader
+WHERE CustomerID BETWEEN 25005 AND 25000;
+
+
+
+
+-- Using NOT BETWEEN
+--1
+SELECT CustomerID, SalesOrderID, OrderDate
+FROM Sales.SalesOrderHeader
+WHERE OrderDate NOT BETWEEN '2005-07-02' AND '2005-07-04';
+--2
+SELECT CustomerID, SalesOrderID, OrderDate
+FROM Sales.SalesOrderHeader
+WHERE CustomerID NOT BETWEEN 25000 AND 25005;
+--3
+SELECT BusinessEntityID, JobTitle
+FROM HumanResources.Employee
+WHERE JobTitle NOT BETWEEN 'C' and 'E';
+--4 An illogical BETWEEN expression
+SELECT CustomerID, SalesOrderID, OrderDate
+FROM Sales.SalesOrderHeader
+WHERE CustomerID NOT BETWEEN 25005 AND 25000;
+
+
+
+
+-- Table Setup for Date/Time Example
+CREATE TABLE #DateTimeExample(
+ ID INT NOT NULL IDENTITY PRIMARY KEY,
+ MyDate DATETIME2(0) NOT NULL,
+ MyValue VARCHAR(25) NOT NULL
+);
+GO
+INSERT INTO #DateTimeExample
+ (MyDate,MyValue)
+VALUES ('2009-01-02 10:30','Bike'),
+ ('2009-01-03 13:00','Trike'),
+ ('2009-01-03 13:10','Bell'),
+ ('2009-01-03 17:35','Seat');
+
+--1 (Doen't return anything, it has to do with the DATETIME2(0) type)
+SELECT ID, MyDate, MyValue
+FROM #DateTimeExample
+WHERE MyDate = '2009-01-03';
+--2
+SELECT ID, MyDate, MyValue
+FROM #DateTimeExample
+WHERE MyDate BETWEEN '2009-01-03 00:00:00' AND '2009-01-03 23:59:59';
+
+
+
+
+
+-- How to Use AND and OR
+--1
+SELECT BusinessEntityID, FirstName, MiddleName, LastName
+FROM Person.Person
+WHERE FirstName = 'Ken' AND LastName = 'Myer';
+--2
+SELECT BusinessEntityID, FirstName, MiddleName, LastName
+FROM Person.Person
+WHERE LastName = 'Myer' OR LastName = 'Meyer';
+
+
+
+
+-- Delete a table if it is not empty
+IF OBJECT_ID('tempdb..#DateTimeExample') IS NOT NULL BEGIN
+ DROP TABLE #DateTimeExample;
+END;
+GO
+
+-- Create a new table again 
+CREATE TABLE #DateTimeExample(
+ ID INT NOT NULL IDENTITY PRIMARY KEY,
+ MyDate DATETIME2(0) NOT NULL,
+ MyValue VARCHAR(25) NOT NULL
+);
+GO
+
+INSERT #DateTimeExample (MyDate, MyValue)
+VALUES ('2009-01-01 10:30','Bike'),
+ ('2009-01-01 11:30','Bike'),
+ ('2009-01-02 13:00','Trike'),
+ ('2009-01-03 13:10','Bell'),
+ ('2009-01-03 17:35','Seat'),
+ ('2009-01-04 00:00','Bike');
+--4
+SELECT ID, MyDate, MyValue
+FROM #DateTimeExample
+WHERE MyDate >= '2009-01-02' AND MyDate < '2009-01-04';
+
+
+
+
+
 
